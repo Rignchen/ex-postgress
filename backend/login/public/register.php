abe50cf59db1933329d88aee7332cc55ec852370
Rignchen <rinchendemort@gmail.com>
2024-03-18 09:08:05 +0100

    feat: querry token

--- backend/login/public/register.php
+++ backend/login/public/register.php
@@ -2,3 +2,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../private/lib.php';
 
+// get the authentication token from the authorization header
+$authHeader = $_SERVER['HTTP_AUTHORIZATION'] ?? '';
+$token = str_replace('Bearer ', '', $authHeader);
