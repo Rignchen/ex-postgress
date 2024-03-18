581d883ed06a6b4b924446b90afa8ffe82a91c52
Rignchen <rinchendemort@gmail.com>
2024-03-18 08:51:32 +0100

    feat: connect to database

--- backend/login/public/login.php
+++ backend/login/public/login.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Symfony\Component\Dotenv;
+
 $data = match ($_SERVER['REQUEST_METHOD']) {
 	'GET' => $_GET,
 	'POST' => $_POST,
@@ -12,3 +14,12 @@ if (!isset($data['username']) || !isset($data['password'])) {
 	echo json_encode(['error' => 'Username and password are required']);
 	exit;
 }
+
+// Load environment variables
+$dotenv = new Dotenv\Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
+
+// Connect to the database
+$dns = "pgsql:host=" . $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME'];
+$pdo = new PDO($dns, $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+
