415056cefb690a62646acd60eee29191c34815ef
Rignchen <rinchendemort@gmail.com>
2024-03-18 09:00:06 +0100

    refactor: extract api responses into a functions

--- backend/login/public/login.php
+++ backend/login/public/login.php
@@ -1,21 +1,18 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../private/lib.php';
 
 use Symfony\Component\Dotenv;
 use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
 
 $data = match ($_SERVER['REQUEST_METHOD']) {
 	'GET' => $_GET,
 	'POST' => $_POST,
-	default => [],
+	default => output(['error' => 'Unsupported method'], 405),
 };
 
-if (!isset($data['username']) || !isset($data['password'])) {
-	http_response_code(400);
-	echo json_encode(['error' => 'Username and password are required']);
-	exit;
-}
+if (!isset($data['username']) || !isset($data['password']))
+    output(['error' => 'Username and password are required'], 400);
 
 // Load environment variables
 $dotenv = new Dotenv\Dotenv();
@@ -30,11 +27,8 @@ $stmt = $pdo->prepare("SELECT * FROM api.users WHERE username = :username");
 $stmt->execute(['username' => $data['username']]);
 $user = $stmt->fetch();
 
-if (!$user || !password_verify($data['password'], $user['password'])) {
-	http_response_code(401);
-	echo json_encode(['error' => 'Invalid username or password']);
-	exit;
-}
+if (!$user || !password_verify($data['password'], $user['password']))
+    output(['error' => 'Invalid username or password'], 401);
 
 // Create a token
 $payload = [
@@ -44,4 +38,4 @@ $payload = [
 ];
 $jwt = JWT::encode($payload, $_ENV['JWT_SECRET'], 'HS256');
 
-echo json_encode(['token' => $jwt]);
+output(['token' => $jwt]);
