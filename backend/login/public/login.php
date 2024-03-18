aa309396f3d5841f8b486297be8902c02c403cb9
Rignchen <rinchendemort@gmail.com>
2024-03-18 08:50:58 +0100

    feat: check if password and username are set

--- backend/login/public/login.php
+++ backend/login/public/login.php
@@ -1,2 +1,14 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$data = match ($_SERVER['REQUEST_METHOD']) {
+	'GET' => $_GET,
+	'POST' => $_POST,
+	default => [],
+};
+
+if (!isset($data['username']) || !isset($data['password'])) {
+	http_response_code(400);
+	echo json_encode(['error' => 'Username and password are required']);
+	exit;
+}
