2994b387e37d9c17387dcb1fb5e3ee90f71daf89
Rignchen <rinchendemort@gmail.com>
2024-03-18 09:39:12 +0100

    refactor: create function to test if token is valid

--- backend/login/private/lib.php
+++ backend/login/private/lib.php
@@ -1,10 +1,27 @@
 <?php
 
 use JetBrains\PhpStorm\NoReturn;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Firebase\JWT\SignatureInvalidException;
 
 #[NoReturn] function output(array $messages, int $code = 200): void {
-    header('Content-Type: application/json');
-    http_response_code($code);
-    echo json_encode($messages);
-    exit;
+	header('Content-Type: application/json');
+	http_response_code($code);
+	echo json_encode($messages);
+	exit;
+}
+function checkToken(string $token, string $secret, array $allowed = []): object {
+	try {
+		$decoded = JWT::decode($token, new Key($secret, 'HS256'));
+	}
+	catch (SignatureInvalidException|DomainException|UnexpectedValueException $e) {
+		match ($e->getMessage()) {
+			'Expired token' => output(['error' => 'The token has expired'], 401),
+			default => output(['error' => 'Invalid authentication token'], 401),
+		};
+	}
+	if (!empty(array_intersect($allowed)) && !(isset($decoded->role) && in_array($decoded->role, $allowed)))
+		output(['error' => 'Forbidden'], 403);
+	return $decoded;
 }
--- backend/login/public/register.php
+++ backend/login/public/register.php
@@ -3,9 +3,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../private/lib.php';
 
 use Symfony\Component\Dotenv;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-use Firebase\JWT\SignatureInvalidException;
 
 // get the authentication token from the authorization header
 $authHeader = $_SERVER['HTTP_AUTHORIZATION'] ?? '';
@@ -16,27 +13,17 @@ $dotenv = new Dotenv\Dotenv();
 $dotenv->load(__DIR__ . '/../.env');
 
 // Decode the token
-try {
-	$decoded = JWT::decode($token, new Key($_ENV['JWT_SECRET'], 'HS256'));
-}
-catch (SignatureInvalidException|DomainException|UnexpectedValueException $e) {
-	match ($e->getMessage()) {
-		'Expired token' => output(['error' => 'The token has expired'], 401),
-		default => output(['error' => 'Invalid authentication token'], 401),
-	};
-}
-if (!isset($decoded->role) || $decoded->role !== 'web_user')
-	output(['error' => 'Forbidden'], 403);
+checkToken($token, $_ENV['JWT_SECRET'], ['web_user']);
 
 // Check if data has been sent
 $data = match ($_SERVER['REQUEST_METHOD']) {
-	'GET' => $_GET,
-	'POST' => $_POST,
-	default => output(['error' => 'Unsupported method'], 405),
+    'GET' => $_GET,
+    'POST' => $_POST,
+    default => output(['error' => 'Unsupported method'], 405),
 };
 
 if (!isset($data['username']) || !isset($data['password']))
-	output(['error' => 'Username and password are required'], 400);
+    output(['error' => 'Username and password are required'], 400);
 
 // Connect to the database
 $dns = "pgsql:host=" . $_ENV['DB_HOST'] . ";port=" . $_ENV['DB_PORT'] . ";dbname=" . $_ENV['DB_NAME'];
@@ -45,13 +32,13 @@ $pdo = new PDO($dns, $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
 // Create a user
 $stmt = $pdo->prepare("INSERT INTO api.users (username, password) VALUES (:username, :password)");
 try {
-	$stmt->execute([
-		'username' => $data['username'],
-		'password' => password_hash($data['password'], PASSWORD_DEFAULT),
-	]);
+    $stmt->execute([
+        'username' => $data['username'],
+        'password' => password_hash($data['password'], PASSWORD_DEFAULT),
+    ]);
 }
 catch (PDOException $e) {
-	output(['error' => 'Username already exists'], 400);
+    output(['error' => 'Username already exists'], 400);
 }
 
 output(['message' => 'User created']);
