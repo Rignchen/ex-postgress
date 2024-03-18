cc2e8c1f1a0fd84e74f794e32667416574ecb7ad
Rignchen <rinchendemort@gmail.com>
2024-03-18 09:11:44 +0100

    feat: check if token is valid

--- backend/login/public/register.php
+++ backend/login/public/register.php
@@ -2,6 +2,26 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../private/lib.php';
 
+use Symfony\Component\Dotenv;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Firebase\JWT\SignatureInvalidException;
+
 // get the authentication token from the authorization header
 $authHeader = $_SERVER['HTTP_AUTHORIZATION'] ?? '';
 $token = str_replace('Bearer ', '', $authHeader);
+
+// Load environment variables
+$dotenv = new Dotenv\Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
+
+// Decode the token
+try {
+	$decoded = JWT::decode($token, new Key($_ENV['JWT_SECRET'], 'HS256'));
+}
+catch (SignatureInvalidException|DomainException|UnexpectedValueException $e) {
+	match ($e->getMessage()) {
+		'Expired token' => output(['error' => 'The token has expired'], 401),
+		default => output(['error' => 'Invalid authentication token'], 401),
+	};
+}
