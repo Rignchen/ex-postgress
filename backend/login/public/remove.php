3174eb652df060b138a0f8fbf97d842292b2ffcf
Rignchen <rinchendemort@gmail.com>
2024-03-18 09:44:29 +0100

    feat: add a page to remove users

---
+++ backend/login/public/remove.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../private/lib.php';
+
+use Symfony\Component\Dotenv;
+
+// get the authentication token from the authorization header
+$authHeader = $_SERVER['HTTP_AUTHORIZATION'] ?? '';
+$token = str_replace('Bearer ', '', $authHeader);
+
+// Load environment variables
+$dotenv = new Dotenv\Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
+
+// Decode the token
+checkToken($token, $_ENV['JWT_SECRET'], ['web_user']);
+
+// Check if data has been sent
+$data = match ($_SERVER['REQUEST_METHOD']) {
+    'GET' => $_GET,
+    'POST' => $_POST,
+    default => output(['error' => 'Unsupported method'], 405),
+};
